7f47230270b19b03a43d35157d9c3e2be7f4b5ab
tscz <tscz@users.noreply.github.com>
2020-11-29 12:40:53 +0100

    Add storybook support

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../projects/ngx-platform-storybook/stories/**/*.stories.mdx",
+    "../projects/ngx-platform-storybook/stories/**/*.stories.@(js|jsx|ts|tsx)"
+  ],
+  "addons": [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials"
+  ]
+}
